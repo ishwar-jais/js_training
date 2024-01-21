d1aa12e70522b62dad744bc284eb4d74d1d472c7
ishwar-jais <157193906+ishwar-jais@users.noreply.github.com>
2024-01-21 11:35:27 +0000

    Conversation Operation

---
+++ .devcontainer/01_basic/03_conversationOperation.js
@@ -0,0 +1,29 @@
+let score = "33abd"
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber = Number(score); // Conversation operation of string to number 
+
+console.log (valueInNumber)
+console.log (typeof (valueInNumber))
+
+//NaN means : not an Number 
+// "33" => 33
+// true => 1 & false => 0
+//"33abc" => NaN
+
+let isLoggedIn = 1
+
+console.log(isLoggedIn)
+
+let booleanisLoggedIn = Boolean (isLoggedIn) // 1 is converted to boolean
+
+console.log (typeof booleanisLoggedIn)
+console.log (booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+
