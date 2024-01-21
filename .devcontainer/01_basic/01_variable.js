035744ed5cb3e69e5e943eb0e7222247c6c725f1
ishwar-jais <157193906+ishwar-jais@users.noreply.github.com>
2024-01-21 10:46:31 +0000

    datatypes

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144552
+let accountEmail ="annijais009@google.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+
+//accountId = 2
+
+accountEmail = "ani.jais02@gmail.com"
+accountPassword = 221232
+accountCity ="Bengaluru"
+
+console.log(accountId);
+console.table([accountId , accountEmail, accountPassword, accountCity]);
---
+++ .devcontainer/01_basic/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+let age =45
+
+// alert (3+3) // we are using nodejs, not browser
+
+//DataType
+         //Primitive DataType
+ //number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+
+ console.log(typeof age); //to know the dataType of variable 
+ console.log(typeof null)
+
+
