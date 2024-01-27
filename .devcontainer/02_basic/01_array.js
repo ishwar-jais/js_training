b68a199a64b219ab427676fdfe0f4f83a0643432
ishwar-jais <157193906+ishwar-jais@users.noreply.github.com>
2024-01-27 13:15:58 +0000

    Array

--- .devcontainer/02_basic/01_array.js
+++ .devcontainer/02_basic/01_array.js
@@ -1,7 +1,7 @@
 //Array 
 
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeros = ["shaktiman", "naagraj"]
+//const myHeros = ["shaktiman", "naagraj"]
 
 //const myArr2 = new Array(1, 2, 3, 4)
 //console.log(myArr[0]);
@@ -19,7 +19,7 @@ const myHeros = ["shaktiman", "naagraj"]
 // console.log(myArr.includes(9));
 
 
-const newArr = myArr.join()  //join method Adds all the elements of an array into a string, separated by the specified separator string.
+//const newArr = myArr.join()  /*join method Adds all the elements of an array into a string,separated by the specified separator string* /
 
 // console.log(myArr);
 // console.log(newArr);
@@ -27,5 +27,15 @@ const newArr = myArr.join()  //join method Adds all the elements of an array int
 
 // ++ slice, splice ///
 
+console.log("A ", myArr);
 
+const myn1 = myArr.slice(0, 5) 
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3) //Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+
+console.log(myn2);
+console.log("C ", myArr);
 
