ed569415560202b21c8ba1bc1b821cc198e1ff8c
ishwar-jais <157193906+ishwar-jais@users.noreply.github.com>
2024-01-27 07:23:11 +0000

    Dates

---
+++ .devcontainer/01_basic/07_dates.js
@@ -0,0 +1,32 @@
+// Dates
+
+//let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2023, 0, 23)
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+//let myCreatedDate = new Date("2023-02-14")
+
+
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toDateString());
+
+//let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate.toLocaleDateString());
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
