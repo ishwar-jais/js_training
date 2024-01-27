e1d1c80825a4b6a8cf3e4fc3838edacc20301a18
ishwar-jais <157193906+ishwar-jais@users.noreply.github.com>
2024-01-27 09:37:48 +0000

    array

---
+++ .devcontainer/02_basic/01_array.js
@@ -0,0 +1,31 @@
+//Array 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+//const myArr2 = new Array(1, 2, 3, 4)
+//console.log(myArr[0]);
+
+// Array methods 
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift(9)
+
+// console.log(myArr.indexOf(4));
+// console.log(myArr.includes(9));
+
+
+const newArr = myArr.join()  //join method Adds all the elements of an array into a string, separated by the specified separator string.
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// ++ slice, splice ///
+
+
+
